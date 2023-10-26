0a18aaedd6eaacc0e145d11d87a828cf1af493da
Kristofer Fangrat <kristofer.fangrat@gmail.com>
2023-10-26 10:33:48 +0200

    publish

--- CompletePythonDeveloper2023FromZero/Python basics/lists.py
+++ CompletePythonDeveloper2023FromZero/Python basics/lists.py
@@ -1,4 +1,11 @@
-amazon_cart = ['notebooks', 'sunglasses', '']
-amazon_cart[0]
+# list slicing
+amazon_cart = [
+    'notebooks', 
+    'sunglasses', 
+    'toys',
+    'grapes'
+    ]
+amazon_cart[0] = 'laptop'
 
-print(amazon_cart[2])
+print(amazon_cart[1:3])
+print(amazon_cart)
